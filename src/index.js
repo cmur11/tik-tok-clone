011a57526b7b4aca17b0e12c133803d518fdfedd
Conor Murnane <conormurnane11@gmail.com>
2021-07-13 22:29:51 -0400

    component heirarchy and layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Upload from "./pages/Upload"
+import './App.css';
+
+function App() {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route component={Upload} path= "/upload" />
+        <Route component={Home} path= "/" />
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +31,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/pages/Upload.js
@@ -0,0 +1,10 @@
+
+function Upload() {
+    return (
+        <div>
+            <h3>Whatsup</h3>
+        </div>
+    )
+}
+
+export default Upload
